767d752d9179841ba34159373f8dff22d99c654a
Alexandra Molins <alemolgut@gmail.com>
2025-11-26 17:59:36 +0100

    CMakeLists changes

--- build/CMakeFiles/programming_project.dir/DependInfo.cmake
+++ build/CMakeFiles/programming_project.dir/DependInfo.cmake
@@ -8,14 +8,14 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/lunalarcher/Programming_project/src/game.c" "CMakeFiles/programming_project.dir/src/game.c.o" "gcc" "CMakeFiles/programming_project.dir/src/game.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/input.c" "CMakeFiles/programming_project.dir/src/input.c.o" "gcc" "CMakeFiles/programming_project.dir/src/input.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/main.c" "CMakeFiles/programming_project.dir/src/main.c.o" "gcc" "CMakeFiles/programming_project.dir/src/main.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/stats.c" "CMakeFiles/programming_project.dir/src/stats.c.o" "gcc" "CMakeFiles/programming_project.dir/src/stats.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/timer.c" "CMakeFiles/programming_project.dir/src/timer.c.o" "gcc" "CMakeFiles/programming_project.dir/src/timer.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/ui_sdl.c" "CMakeFiles/programming_project.dir/src/ui_sdl.c.o" "gcc" "CMakeFiles/programming_project.dir/src/ui_sdl.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/utils.c" "CMakeFiles/programming_project.dir/src/utils.c.o" "gcc" "CMakeFiles/programming_project.dir/src/utils.c.o.d"
-  "/Users/lunalarcher/Programming_project/src/word_list.c" "CMakeFiles/programming_project.dir/src/word_list.c.o" "gcc" "CMakeFiles/programming_project.dir/src/word_list.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/game.c" "CMakeFiles/programming_project.dir/src/game.c.o" "gcc" "CMakeFiles/programming_project.dir/src/game.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/input.c" "CMakeFiles/programming_project.dir/src/input.c.o" "gcc" "CMakeFiles/programming_project.dir/src/input.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/main.c" "CMakeFiles/programming_project.dir/src/main.c.o" "gcc" "CMakeFiles/programming_project.dir/src/main.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/stats.c" "CMakeFiles/programming_project.dir/src/stats.c.o" "gcc" "CMakeFiles/programming_project.dir/src/stats.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/timer.c" "CMakeFiles/programming_project.dir/src/timer.c.o" "gcc" "CMakeFiles/programming_project.dir/src/timer.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/ui_sdl.c" "CMakeFiles/programming_project.dir/src/ui_sdl.c.o" "gcc" "CMakeFiles/programming_project.dir/src/ui_sdl.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/utils.c" "CMakeFiles/programming_project.dir/src/utils.c.o" "gcc" "CMakeFiles/programming_project.dir/src/utils.c.o.d"
+  "/Users/alexandramolinsmacbook/Programming_project/src/word_list.c" "CMakeFiles/programming_project.dir/src/word_list.c.o" "gcc" "CMakeFiles/programming_project.dir/src/word_list.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/CMakeFiles/programming_project.dir/src/stats.c.o.d
+++ build/CMakeFiles/programming_project.dir/src/stats.c.o.d
@@ -1,3 +1,3 @@
 CMakeFiles/programming_project.dir/src/stats.c.o: \
-  /Users/lunalarcher/Programming_project/src/stats.c \
-  /Users/lunalarcher/Programming_project/include/stats.h
+  /Users/alexandramolinsmacbook/Programming_project/src/stats.c \
+  /Users/alexandramolinsmacbook/Programming_project/include/stats.h
