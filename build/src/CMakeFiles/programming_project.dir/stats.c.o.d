86535011ef578c3a67b0337962855d9add125d6a
sanadhamad <sanadhamad2006@gmail.com>
2025-11-28 17:38:36 +0100

    sanads changes

--- build/src/CMakeFiles/programming_project.dir/stats.c.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/programming_project.dir/stats.c.o: \
-  /Users/alexandramolinsmacbook/Programming_project/src/stats.c \
-  /Users/alexandramolinsmacbook/Programming_project/include/stats.h
